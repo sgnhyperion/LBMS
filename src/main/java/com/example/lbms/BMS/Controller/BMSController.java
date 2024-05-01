203e75b308aea73051fc7e930e79b4afe3c4f04e
bhavya jain <bhavyajain4274@gmail.com>
2024-05-02 02:52:20 +0530

    PMS Commited by Arjun

--- src/main/java/com/example/lbms/BMS/Controller/BMSController.java
+++ src/main/java/com/example/lbms/BMS/Controller/BMSController.java
@@ -12,60 +12,60 @@ import java.util.Optional;
 @RequestMapping("/books")
 public class BMSController {
 
-        private final BookService bookService;
+    private final BookService bookService;
 
-        BMSController(BookService bookService){
-            this.bookService = bookService;
-        }
-//        Search By title, Author Name And Genre Name
-        @GetMapping("/title/{title}")
-        public List<Optional<Book>> searchByTitle(@PathVariable("title") String title){
-            return bookService.searchByTitle(title);
-        }
+    BMSController(BookService bookService){
+        this.bookService = bookService;
+    }
+    //        Search By title, Author Name And Genre Name
+    @GetMapping("/title/{title}")
+    public List<Optional<Book>> searchByTitle(@PathVariable("title") String title){
+        return bookService.searchByTitle(title);
+    }
 
-        @GetMapping("/author/{name}")
-        public List<Optional<Book>> searchByAuthor(@PathVariable("name") String authorName){
-            return bookService.searchByAuthor(authorName);
-        }
+    @GetMapping("/author/{name}")
+    public List<Optional<Book>> searchByAuthor(@PathVariable("name") String authorName){
+        return bookService.searchByAuthor(authorName);
+    }
 
-        @GetMapping("/genre/{genre}")
-        public List<Optional<Book>> searchByGenre(@PathVariable("genre") String genreName){
-            Genre genre = Genre.valueOf(genreName);
-            return bookService.searchByGenre(genre);
-        }
-// Create A book
-        @PostMapping("/")
-        public Book addBook(@RequestBody Book book){
-            return bookService.addBook(book);
-        }
+    @GetMapping("/genre/{genre}")
+    public List<Optional<Book>> searchByGenre(@PathVariable("genre") String genreName){
+        Genre genre = Genre.valueOf(genreName);
+        return bookService.searchByGenre(genre);
+    }
+    // Create A book
+    @PostMapping("/")
+    public Book addBook(@RequestBody Book book){
+        return bookService.addBook(book);
+    }
 
-//      Update A Book using patch
-        @PatchMapping("/{id}")
-        public Book updateBook(@PathVariable("id") Long id, @RequestBody Book book){
-            return bookService.updateBook(id, book);
-        }
+    //      Update A Book using patch
+    @PatchMapping("/{id}")
+    public Book updateBook(@PathVariable("id") Long id, @RequestBody Book book){
+        return bookService.updateBook(id, book);
+    }
 
-//      Delete a Book
-        @DeleteMapping("/{id}")
-        public Book deleteBook(@PathVariable("id") Long id){
-            return bookService.deleteBook(id);
-        }
+    //      Delete a Book
+    @DeleteMapping("/{id}")
+    public Book deleteBook(@PathVariable("id") Long id){
+        return bookService.deleteBook(id);
+    }
 
-//      Get a Book by Id
-        @GetMapping("/{id}")
-        public Book getSingleBook(@PathVariable("id") Long id)
-        {
-            return bookService.getSingleBook(id);
+    //      Get a Book by Id
+    @GetMapping("/{id}")
+    public Book getSingleBook(@PathVariable("id") Long id)
+    {
+        return bookService.getSingleBook(id);
 
-        }
+    }
 
-//        Get all Books
-        @GetMapping("/")
-        public List<Book> getAllBooks()
-        {
-            return bookService.getAllBooks();
+    //        Get all Books
+    @GetMapping("/")
+    public List<Book> getAllBooks()
+    {
+        return bookService.getAllBooks();
 
-        }
+    }
 
 
 }
--- src/main/java/com/example/lbms/BMS/Model/Book.java
+++ src/main/java/com/example/lbms/BMS/Model/Book.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,7 +19,7 @@ public class Book {
     private String title;
     private String author;
     private Genre genre;
-//    @ManyToOne
+    //    @ManyToOne
 //    private List<Long> patron;
     private Long isbn;
     private BookStatus bookStatus;
